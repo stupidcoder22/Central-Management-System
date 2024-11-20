fe7a33f01bb056e13fcc889a9cd03fd63fcef68f
Prateek Singh <stupidcoder22@gmail.com>
2024-11-21 01:41:56 +0530

    some changes

--- src/scenes/CheckerVerification/Checkertabledata.jsx
+++ src/scenes/CheckerVerification/Checkertabledata.jsx
@@ -1,22 +1,28 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { styled } from "@mui/material/styles";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
+import {
+  Grid,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  Typography,
+} from "@mui/material";
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
   fontWeight: 700,
-  backgroundColor: theme.palette.grey[200],
+  backgroundColor: "#3f51b5", // Blue background for header
+  color: "#fff", // White text for header to contrast with blue
+  padding: theme.spacing(1), // Custom padding
 }));
 
 const StyledTableRow = styled(TableRow)(({ theme }) => ({
   "&:nth-of-type(odd)": {
-    backgroundColor: theme.palette.action.hover,
+    backgroundColor: theme.palette.action.hover, // Hover effect for odd rows
   },
 }));
 
@@ -29,29 +35,34 @@ const CheckerTableData = () => {
   );
 
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 700 }} aria-label="product table">
-        <TableHead>
-          <TableRow>
-            {headers.map((header) => (
-              <StyledTableCell align="center" key={header}>
-                {header}
-              </StyledTableCell>
-            ))}
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {productData.map((product) => (
-            <StyledTableRow key={product.id}>
-              <TableCell align="center">{product.id}</TableCell>
-              <TableCell align="center">{product.name}</TableCell>
-              <TableCell align="center">{product.billingStreet}</TableCell>
-              <TableCell align="center">{product.billingCity}</TableCell>
-            </StyledTableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <Grid container>
+      <Grid item xs={12}></Grid>
+      <Grid item xs={12}>
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 700 }} aria-label="product table">
+            <TableHead>
+              <TableRow>
+                {headers.map((header) => (
+                  <StyledTableCell align="center" key={header}>
+                    {header}
+                  </StyledTableCell>
+                ))}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {productData.map((product) => (
+                <StyledTableRow key={product.id}>
+                  <TableCell align="center">{product.id}</TableCell>
+                  <TableCell align="center">{product.name}</TableCell>
+                  <TableCell align="center">{product.billingStreet}</TableCell>
+                  <TableCell align="center">{product.billingCity}</TableCell>
+                </StyledTableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Grid>
+    </Grid>
   );
 };
 
